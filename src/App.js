bbb5246877a117a7070c52d44ac86dc442aa978c
ARJUN-SABU <arjunsabu99@gmail.com>
2022-12-23 11:55:37 +0530

    Added the basic cake feature folder and files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 
+//components
+import CakeView from "./features/cake/CakeView";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <CakeView />
     </div>
   );
 }
---
+++ src/features/cake/CakeView.js
@@ -0,0 +1,11 @@
+function CakeView() {
+  return (
+    <div>
+      <h1>Number of cakes: {10}</h1>
+      <button>Add Cake</button>
+      <button>Order Cake</button>
+    </div>
+  );
+}
+
+export default CakeView;
